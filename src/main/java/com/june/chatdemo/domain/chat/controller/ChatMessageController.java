e76dc60fdbb21ad4e67752635ea181c0ef472c24
hi-june <wnsgur735@gmail.com>
2023-04-17 22:38:17 +0900

    refactor: channelTopic과 redisListener 단일화

--- src/main/java/com/june/chatdemo/domain/chat/controller/ChatMessageController.java
+++ src/main/java/com/june/chatdemo/domain/chat/controller/ChatMessageController.java
@@ -1,27 +1,24 @@
 package com.june.chatdemo.domain.chat.controller;
 
 import com.june.chatdemo.domain.chat.model.ChatMessage;
-import com.june.chatdemo.domain.chat.model.RedisPublisher;
-import com.june.chatdemo.domain.chat.repository.ChatRoomRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
 
 @RequiredArgsConstructor
 @Controller
 public class ChatMessageController {
-    private final RedisPublisher redisPublisher;
-    private final ChatRoomRepository chatRoomRepository;
+    private final RedisTemplate<String, Object> redisTemplate;
+    private final ChannelTopic channelTopic;
 
     @MessageMapping("/chat/message")
     public void message(ChatMessage chatMessage) {
         if (ChatMessage.MessageType.ENTER.equals(chatMessage.getType())) {
-            chatRoomRepository.enterChatRoom(chatMessage.getRoomId());
             chatMessage.setMessage(chatMessage.getSender() + "님 등장!");
         }
 
-        // Websocket에 발행된 메시지를 redis로 발행한다(publish)
-        // publisher에 topic과 chatMessage를 전달
-        redisPublisher.publish(chatRoomRepository.getTopic(chatMessage.getRoomId()), chatMessage);
+        redisTemplate.convertAndSend(channelTopic.getTopic(), chatMessage);
     }
 }
--- src/main/java/com/june/chatdemo/global/config/RedisConfig.java
+++ src/main/java/com/june/chatdemo/global/config/RedisConfig.java
@@ -1,15 +1,29 @@
 package com.june.chatdemo.global.config;
 
+import com.june.chatdemo.domain.chat.model.RedisSubscriber;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+@RequiredArgsConstructor
 @Configuration
 public class RedisConfig {
+
+    /**
+     * 단일 Topic 사용을 위한 Bean 설정
+     */
+    @Bean
+    public ChannelTopic channelTopic() {
+        return new ChannelTopic("chatroom");
+    }
+
     /**
      * redis pub/sub 메시지를 처리하는 listener 설정
      *
@@ -17,12 +31,22 @@ public class RedisConfig {
      * @return listenerContainer
      */
     @Bean
-    public RedisMessageListenerContainer redisMessageListener(RedisConnectionFactory connectionFactory) {
+    public RedisMessageListenerContainer redisMessageListener(RedisConnectionFactory connectionFactory,
+                                                              MessageListenerAdapter listenerAdapter,
+                                                              ChannelTopic channelTopic) {
         RedisMessageListenerContainer container = new RedisMessageListenerContainer();
         container.setConnectionFactory(connectionFactory);
+        container.addMessageListener(listenerAdapter, channelTopic);    // 해당 channel에 message가 들어왔을 때 적용할 리스너를 리스터 어뎁터를 통해 등록함
         return container;
     }
 
+    /**
+     * 실제 메시지를 처리하는 subscriber 설정 추가
+     */
+    @Bean
+    public MessageListenerAdapter listenerAdapter(RedisSubscriber subscriber) {
+        return new MessageListenerAdapter(subscriber, "sendMessage");
+    }
 
     /**
      * 어플리케이션에서 사용할 redisTemplate 설정
