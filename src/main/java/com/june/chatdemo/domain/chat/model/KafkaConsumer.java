88c4db0669d3508c4659ea9e6220ad365f04c43d
hi-june <wnsgur735@gmail.com>
2023-05-13 23:27:00 +0900

    feat: kafka consumer 정의

--- src/main/java/com/june/chatdemo/domain/chat/model/KafkaConsumer.java
+++ src/main/java/com/june/chatdemo/domain/chat/model/KafkaConsumer.java
@@ -1,24 +1,23 @@
 package com.june.chatdemo.domain.chat.model;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @RequiredArgsConstructor
 @Service
-public class RedisSubscriber {
-    private final ObjectMapper objectMapper;
+public class KafkaConsumer {
     private final SimpMessageSendingOperations messagingTemplate;
 
     /**
-     * Redis에서 메시지가 발행(publish)되면 대기하고 있던 Redis Subscriber가 해당 메시지를 받아 처리한다.
+     * Kafka에서 메시지가 발행(publish)되면 대기하고 있던 Kafka Consumer가 해당 메시지를 받아 처리한다.
      */
-    public void sendMessage(String publishMessage) {
+    @KafkaListener(topics = "${spring.kafka.template.default-topic}", groupId = "${spring.kafka.consumer.group-id}")
+    public void sendMessage(ChatMessage chatMessage) {
         try {
-            ChatMessage chatMessage = objectMapper.readValue(publishMessage, ChatMessage.class);    // ChatMessage 객채로 맵핑
             messagingTemplate.convertAndSend("/sub/chat/room/" + chatMessage.getRoomId(), chatMessage); // Websocket 구독자에게 채팅 메시지 Send
         } catch (Exception e) {
             log.error(e.getMessage());
